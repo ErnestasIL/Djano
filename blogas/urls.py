a4c205d00434b16d7986f253ecc2a3f544c26189
Ernestas Ilgevicius <ernestas963@yahoo.com>
2025-03-24 21:41:17 +0200

    task done, i think

---
+++ blogas/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.pagrindinis , name='pagrindinis'),
+    path('apie/', views.apie, name='apie'),
+    path('kontaktai/', views.kontaktai, name='kontaktai'),
+    path('naujienos', views.naujienos, name='naujienos'),
+    path('author', views.author, name='author')
+]
+
--- blogas/views.py
+++ blogas/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def pagrindinis(request):
+    kontekstas = {
+        'pavadinimas': 'Mano Puslapis',
+        'antraste': 'Welcome to my main page'
+    }
+    return render(request, 'pagrindinis.html', kontekstas)
+
+def apie(request):
+    context = {
+        'pavadinimas': 'Apie mus',
+        'antraste': 'Apacioje rasite texta'
+    }
+    return render(request, 'apie.html', context)
+
+def kontaktai(request):
+    return HttpResponse('You can contact us on this dead url i havent even posted')
+
+def naujienos(request):
+    content = {
+        'pavadinimas': 'Naujienos',
+        'antraste': 'Svieziausios naujienos!'
+    }
+    return render(request, 'naujienos.html', content)
+
+def author(request):
+    context = {
+        'vardas': 'Jonas',
+        'pomegiai': [
+            'Coding', 'Coffee', 'randomly changing language mid task', 'bikes'
+        ]
+    }
+    return render(request, 'author.html', context)
